229292956ccdc73f28595e280e1abcd6bf416f5e
Kola Kolade <kolakolade@gmail.com>
2022-11-15 22:10:54 +0100

    Created the work section body

--- main.js
+++ main.js
@@ -1,63 +1,57 @@
-const menuItems = document.querySelectorAll('.nav-center .links li');
-const sectionFooters = document.querySelectorAll('section, footer');
-const navToggle = document.querySelector('.nav-toggle');
-const links = document.querySelector('.links');
+const menuItems = document.querySelectorAll(".nav-center .links li");
+const sectionFooters = document.querySelectorAll("section, footer");
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
 // const workSection = document.getElementById('projects')
 
 // eslint-disable-next-line no-undef
-icon = navToggle.querySelector('.fa-times');
+icon = navToggle.querySelector(".fa-times");
 // eslint-disable-next-line no-undef
-openIcon = navToggle.querySelector('.fa-bars');
+openIcon = navToggle.querySelector(".fa-bars");
 
-navToggle.addEventListener('click', () => {
-  links.classList.toggle('show-links');
+navToggle.addEventListener("click", () => {
+  links.classList.toggle("show-links");
   // eslint-disable-next-line no-undef
-  icon.classList.toggle('close-icon');
+  icon.classList.toggle("close-icon");
   // eslint-disable-next-line no-undef
-  openIcon.classList.toggle('open-icon');
+  openIcon.classList.toggle("open-icon");
 });
 
-window.addEventListener('scroll', () => {
-  let currentSection = '';
+window.addEventListener("scroll", () => {
+  let currentSection = "";
 
   sectionFooters.forEach((sectionFooter) => {
     const sectionTop = sectionFooter.offsetTop;
     // eslint-disable-next-line no-restricted-globals
-    if (pageYOffset >= (sectionTop)) {
-      currentSection = sectionFooter.getAttribute('id');
+    if (pageYOffset >= sectionTop) {
+      currentSection = sectionFooter.getAttribute("id");
     }
   });
   menuItems.forEach((menuItem) => {
-    menuItem.classList.remove('active');
+    menuItem.classList.remove("active");
     if (menuItem.classList.contains(currentSection)) {
-      menuItem.classList.add('active');
+      menuItem.classList.add("active");
     }
-    menuItem.addEventListener('click', () => {
-      links.classList.toggle('show-links');
+    menuItem.addEventListener("click", () => {
+      links.classList.toggle("show-links");
     });
-    links.classList.remove('show-links');
+    links.classList.remove("show-links");
     // eslint-disable-next-line no-undef
-    icon.classList.remove('close-icon');
+    icon.classList.remove("close-icon");
     // eslint-disable-next-line no-undef
-    openIcon.classList.remove('open-icon');
+    openIcon.classList.remove("open-icon");
   });
-});
-
-window.addEventListener('load', () => {
-  // projectTitle.innerText = 'Project Title';
-  // eslint-disable-next-line no-console
-  // console.log(projectTitle);
 });
 
 // eslint-disable-next-line no-unused-vars
 const workSection = [
   {
     id: 1,
-    name: 'Tonic',
-    companies: ['Code', 'CSS', 2015],
-    img: './Images/Snapshoot Portfolio__.png',
-    desc: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
-    technologies: ['html', 'css', 'javaScript'],
+    name: "Tonic",
+    companies: ["Code", "CSS", 2015],
+    img: "./Images/Snapshoot Portfolio__.png",
+    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required",
+    technologies: ["html", "css", "javaScript"],
   },
 ];
 
@@ -85,35 +79,38 @@ const workSection = [
 //   ulList.append(liList);
 // }
 
-window.addEventListener('DOMContentLoaded',() =>{
+window.addEventListener("DOMContentLoaded", () => {
   // eslint-disable-next-line camelcase, no-unused-vars
-  const projectSection = document.getElementById('projects');
+  const projectSection = document.getElementById("projects");
   // projectSection.classList.add('work-section');
 
-  const projectsCard = document.createElement('div');
-  projectsCard.classList.add('projects-card');
+  const projectsCard = document.createElement("div");
+  projectsCard.classList.add("projects-card");
 
-  const cardHeader = document.createElement('div');
-  cardHeader.classList.add('card-header');
+  const cardHeader = document.createElement("div");
+  cardHeader.classList.add("card-header");
 
-  const projects = document.createElement('div');
-  projects.classList.add('projects');
+  const projects = document.createElement("div");
+  projects.classList.add("projects");
 
-  const cardBody = document.createElement('div');
-  cardBody.classList.add('card-body');
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
 
-  const authorRoleCompany = document.createElement('div');
-  authorRoleCompany.classList.add('author-role-company');
+  const authorRoleCompany = document.createElement("div");
+  authorRoleCompany.classList.add("author-role-company");
 
-  const projectTitle = document.createElement('div');
-  projectTitle.classList.add('project-title');
+  const projectTitle = document.createElement("div");
+  projectTitle.classList.add("project-title");
 
   // eslint-disable-next-line no-unused-vars
-  const ulLinkItems = document.createElement('ul');
+  const ulLinkItems = document.createElement("ul");
+
+  const workDescription = document.createElement("div");
+  workDescription.classList.add("work-description");
 
   // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < (workSection[0].companies).length; i++) {
-    const listItems = document.createElement('li');
+  for (let i = 0; i < workSection[0].companies.length; i++) {
+    const listItems = document.createElement("li");
     listItems.innerText = workSection[0].companies[i];
     ulLinkItems.append(listItems);
   }
