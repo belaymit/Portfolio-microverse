1b2302acb5db0a42fecaf0281aa8f9fd125d1ec9
belaymit <2belamit@gmail.com>
2022-11-17 23:05:34 +0300

    added localstorage reference

--- formValidation.js
+++ formValidation.js
@@ -5,7 +5,14 @@ const regEx = /^[a-z0-9]+@[a-z0-9-]+\.[a-z0-9-.]+$/;
 
 function validate(e) {
   const emailVal = email.value;
+  const formData = {
+    fullName: document.querySelector('#name-input').value,
+    emailAddress: document.querySelector('#email-input').value,
+    message: document.querySelector('.textarea').value,
+  };
+
   if (emailVal.match(regEx)) {
+    localStorage.setItem('DATA', JSON.stringify(formData));
     error.style.display = 'none';
     email.style.border = '1px solid #d0d9d4';
     return true;
