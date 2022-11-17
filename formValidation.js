b01ff35e4b31be1c0ea0c4d796280cb7a889873b
Kola Kolade <kolakolade@gmail.com>
2022-11-17 15:55:44 +0100

    Added the error message and code to generate the error

--- formValidation.js
+++ formValidation.js
@@ -1,16 +1,20 @@
-const form = document.querySelector('.contact-form');
-const email = document.querySelector('#email-input');
-const error = document.querySelector('.error');
+const form = document.querySelector(".contact-form");
+const email = document.querySelector("#email-input");
+const error = document.querySelector(".error");
 const regEx = /^[a-z0-9]+@[a-z0-9-]+\.[a-z0-9-.]+$/;
 
 function validate(e) {
   const emailVal = email.value;
   if (emailVal.match(regEx)) {
-    error.style.display = 'none';
-    email.style.border = '1px solid #d0d9d4';
+    error.style.display = "none";
+    email.style.border = "1px solid #d0d9d4";
     return true;
+  } else {
+    e.preventDefault();
+    error.style.display = "block";
+    error.textContent =
+      "The email address should contain only lowercase letters ";
+    return false;
   }
-  e.preventDefault();
-  return false;
 }
-form.addEventListener('submit', validate);
+form.addEventListener("submit", validate);
