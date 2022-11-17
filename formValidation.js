81b5f04bd864d75c1d657ba74d1068d8e048a5f0
belaymit <2belamit@gmail.com>
2022-11-17 23:13:30 +0300

    removed all linter errors

--- formValidation.js
+++ formValidation.js
@@ -1,37 +1,37 @@
-const form = document.querySelector(".contact-form");
-const email = document.querySelector("#email-input");
-const error = document.querySelector(".error");
+const form = document.querySelector('.contact-form');
+const email = document.querySelector('#email-input');
+const error = document.querySelector('.error');
 const regEx = /^[a-z0-9]+@[a-z0-9-]+\.[a-z0-9-.]+$/;
 
 function validate(e) {
   const emailVal = email.value;
   const formData = {
-    fullName: document.querySelector("#name-input").value,
-    emailAddress: document.querySelector("#email-input").value,
-    message: document.querySelector(".textarea").value,
+    fullName: document.querySelector('#name-input').value,
+    emailAddress: document.querySelector('#email-input').value,
+    message: document.querySelector('.textarea').value,
   };
 
   if (emailVal.match(regEx)) {
-    localStorage.setItem("DATA", JSON.stringify(formData));
-    error.style.display = "none";
-    email.style.border = "1px solid #d0d9d4";
+    localStorage.setItem('DATA', JSON.stringify(formData));
+    error.style.display = 'none';
+    email.style.border = '1px solid #d0d9d4';
     return true;
   }
-  error.style.display = "block";
-  error.innerHTML = "Email should only contain lowercase letters";
-  email.style.border = "1px solid rgb(204, 11, 11)";
+  error.style.display = 'block';
+  error.innerHTML = 'Email should only contain lowercase letters';
+  email.style.border = '1px solid rgb(204, 11, 11)';
   e.preventDefault();
   return false;
 }
 
-form.addEventListener("submit", validate);
-const getInfo = localStorage.getItem("DATA");
+form.addEventListener('submit', validate);
+const getInfo = localStorage.getItem('DATA');
 const getContactInfo = JSON.parse(getInfo);
 
-window.addEventListener("load", () => {
-  if (localStorage.getItem("DATA")) {
-    document.querySelector("#name-input").value = getContactInfo.fullName;
-    document.querySelector("#email-input").value = getContactInfo.emailAddress;
-    document.querySelector(".textarea").value = getContactInfo.message;
+window.addEventListener('load', () => {
+  if (localStorage.getItem('DATA')) {
+    document.querySelector('#name-input').value = getContactInfo.fullName;
+    document.querySelector('#email-input').value = getContactInfo.emailAddress;
+    document.querySelector('.textarea').value = getContactInfo.message;
   }
 });
