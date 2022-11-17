91ef3c349546d2ecf9fac8840f03cbae3a057511
belaymit <2belamit@gmail.com>
2022-11-17 17:57:57 +0300

    added default preventer

--- formValidation.js
+++ formValidation.js
@@ -1,20 +1,20 @@
-const form = document.querySelector(".contact-form");
-const email = document.querySelector("#email-input");
-const error = document.querySelector(".error");
+const form = document.querySelector('.contact-form');
+const email = document.querySelector('#email-input');
+const error = document.querySelector('.error');
 const regEx = /^[a-z0-9]+@[a-z0-9-]+\.[a-z0-9-.]+$/;
 
 function validate(e) {
   const emailVal = email.value;
   if (emailVal.match(regEx)) {
-    error.style.display = "none";
-    email.style.border = "1px solid #d0d9d4";
+    error.style.display = 'none';
+    email.style.border = '1px solid #d0d9d4';
     return true;
-  } else {
-    e.preventDefault();
-    error.style.display = "block";
-    error.textContent =
-      "The email address should contain only lowercase letters ";
-    return false;
   }
+  error.style.display = 'block';
+  error.innerHTML = 'Email should only contain lowercase letters';
+  email.style.border = '1px solid rgb(204, 11, 11)';
+  e.preventDefault();
+  return false;
 }
-form.addEventListener("submit", validate);
+
+form.addEventListener('submit', validate);
